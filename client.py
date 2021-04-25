5847437d087811e66cc88589a6e8adb89abfabfb
clibc <shirvani.abddullayev@gmail.com>
2021-04-25 22:37:34 +0200

    receiving and sending data

--- client.py
+++ client.py
@@ -1,19 +1,22 @@
-import socket
-import sys
-import pickle
+import socket, sys, pickle, time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.1.19', 4444))
+s.connect(('192.168.1.19', 1111))
 
 class sendthis():
+    x = 0
+    y = 0
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    x = 0
-    y = 0
 
 while True:
     data = s.recv(89)
     data = pickle.loads(data)
-
     print("Enemy position: ", data.x, data.y)
+    data.x += 1
+    data.y += 1
+    s.send(pickle.dumps(data))
+    time.sleep(0.01)
+    
+s.close()
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-import socket, pickle, sys
+import socket, pickle, sys, time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.1.19', 4444))
+s.bind(('192.168.1.19', 1111))
 
-s.listen(2)
+s.listen(1)
 
 class sendthis():
     def __init__(self, x, y):
@@ -15,10 +15,17 @@ class sendthis():
 while True:
     conn,addr = s.accept()
     s = sendthis(10,10)
-    #print(sys.getsizeof(serialized))
-
     while True:
+        serialized = pickle.dumps(s) 
         s.x += 1
         s.y += 1
-        serialized = pickle.dumps(s) 
         conn.send(serialized)
+        data = conn.recv(89)
+        data = pickle.loads(data)
+
+        s = data
+        print("data come in :", data.x, data.y)
+        time.sleep(0.01)
+
+
+conn.close()
