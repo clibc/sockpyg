f5f3628c07f12c2c4e717a9713fd3de8ccd13f54
clibc <shirvani.abddullayev@gmail.com>
2021-04-30 16:10:43 +0200

    playable demo

--- client.py
+++
@@ -1,8 +0,0 @@
-import networking
-
-s = networking.Server('192.168.1.15', 4444, False)
-
-while True:
-    data = s.Receive(1000)
-    string = input("You:")
-    s.Send(string)
--- server.py
+++
@@ -1,9 +0,0 @@
-import networking
-
-s = networking.Server('192.168.1.15', 4444, True)
-
-while True:
-    string = input("You:")
-    s.Send(string)
-    data = s.Receive(1000)
-    print("Other: ", data)
